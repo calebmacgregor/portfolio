bb1f3848b68f37aca48f14304ba2a50ed214978f
Caleb MacGregor <calebsmacgregor@gmail.com>
2021-10-12 17:58:51 +1100

    Moved nav to top-down

--- main.js
+++ main.js
@@ -15,9 +15,19 @@ function handleNavExpander(e) {
 	navList.classList.toggle("mobile-hidden")
 }
 
+function minimiseNav(e) {
+	if (!e.target.classList.contains("nav-item")) return
+	const navList = document.querySelector(".nav-list")
+	const navExpander = document.querySelector(".nav-expander")
+
+	navList.classList.add("mobile-hidden")
+	navExpander.classList.remove("expanded")
+}
+
 document.addEventListener("click", handleImageZoom)
 document.addEventListener("click", expandScreenshotModalMobile)
 document.addEventListener("click", expandScreenshotModalDesktop)
 document.addEventListener("click", minimizeScreenshotModal)
 document.addEventListener("click", handleNavExpander)
+document.addEventListener("click", minimiseNav)
 // document.addEventListener("scroll", autohideNav)
