5840f4b963fa3e3bf8f08945a969caf02bde09b2
Caleb MacGregor <calebsmacgregor@gmail.com>
2022-09-20 21:57:32 +1000

    Updated to use new api link

--- public/populate.js
+++ public/populate.js
@@ -1,149 +1,142 @@
 const BASE_URL = "https://seal-app-7asfv.ondigitalocean.app/api/"
 
 async function populateProject() {
-  //Get all projects
-  fetch(BASE_URL)
+	//Get all projects
+	fetch(BASE_URL)
 }
 
 async function fetchData(endpoint, params) {
-  const output = await fetch(`${BASE_URL}${endpoint}?${params}`).then(
-    (response) => response.json()
-  )
+	const output = await fetch(`${BASE_URL}${endpoint}?${params}`).then(
+		(response) => response.json()
+	)
 
-  return output
+	return output
 }
 
 async function populateProject(response) {
-  const data = await response.data
-  let iterator = 2
-  data.forEach((project) => {
-    const section = document.createElement("section")
-    section.classList = "project subsection"
+	const data = await response.data
+	let iterator = 2
+	data.forEach((project) => {
+		const section = document.createElement("section")
+		section.classList = "project subsection"
 
-    const projectScreenshotContainer = document.createElement("div")
-    projectScreenshotContainer.classList = `project-screenshot-container ${
-      iterator % 2 === 0 ? "left" : "right"
-    }`
-    section.appendChild(projectScreenshotContainer)
+		const projectScreenshotContainer = document.createElement("div")
+		projectScreenshotContainer.classList = `project-screenshot-container ${
+			iterator % 2 === 0 ? "left" : "right"
+		}`
+		section.appendChild(projectScreenshotContainer)
 
-    // Loop through every screenshot
-    project.attributes.screenshots.data.forEach((screenshot) => {
-      const screenshotElement = document.createElement("img")
-      screenshotElement.classList = "screenshot"
-      screenshot.alt = ""
-      screenshotElement.src = `https://seal-app-7asfv.ondigitalocean.app${screenshot.attributes.url}`
-      console.log(screenshot.attributes)
+		// Loop through every screenshot
+		project.attributes.screenshots.data.forEach((screenshot) => {
+			const screenshotElement = document.createElement("img")
+			screenshotElement.classList = "screenshot"
+			screenshot.alt = ""
+			screenshotElement.src = `https://seal-app-7asfv.ondigitalocean.app${screenshot.attributes.url}`
+			console.log(screenshot.attributes)
 
-      projectScreenshotContainer.appendChild(screenshotElement)
-    })
+			projectScreenshotContainer.appendChild(screenshotElement)
+		})
 
-    const projectTextContainer = document.createElement("div")
-    projectTextContainer.classList = `project-text-container ${
-      iterator % 2 === 0 ? "left" : "right"
-    }`
-    section.appendChild(projectTextContainer)
+		const projectTextContainer = document.createElement("div")
+		projectTextContainer.classList = `project-text-container ${
+			iterator % 2 === 0 ? "left" : "right"
+		}`
+		section.appendChild(projectTextContainer)
 
-    const projectTitleContainer = document.createElement("div")
-    projectTitleContainer.classList = `project-title-container ${
-      iterator % 2 === 0 ? "left" : "right"
-    }`
-    projectTextContainer.appendChild(projectTitleContainer)
+		const projectTitleContainer = document.createElement("div")
+		projectTitleContainer.classList = `project-title-container ${
+			iterator % 2 === 0 ? "left" : "right"
+		}`
+		projectTextContainer.appendChild(projectTitleContainer)
 
-    if (project.attributes.logo.data) {
-      const projectIcon = document.createElement("img")
-      projectIcon.classList = "project-icon"
-      projectIcon.src = `https://seal-app-7asfv.ondigitalocean.app${project.attributes.logo.data?.attributes.url}`
-      projectTitleContainer.appendChild(projectIcon)
-    }
+		if (project.attributes.logo.data) {
+			const projectIcon = document.createElement("img")
+			projectIcon.classList = "project-icon"
+			projectIcon.src = `https://seal-app-7asfv.ondigitalocean.app${project.attributes.logo.data?.attributes.url}`
+			projectTitleContainer.appendChild(projectIcon)
+		}
 
-    const projectTitle = document.createElement("h3")
-    projectTitle.classList = `project-title ${
-      iterator % 2 === 0 ? "left" : "right"
-    }`
-    projectTitle.innerText = project.attributes.title
-    projectTitleContainer.appendChild(projectTitle)
+		const projectTitle = document.createElement("h3")
+		projectTitle.classList = `project-title ${
+			iterator % 2 === 0 ? "left" : "right"
+		}`
+		projectTitle.innerText = project.attributes.title
+		projectTitleContainer.appendChild(projectTitle)
 
-    const projectDescription = document.createElement("p")
-    projectDescription.classList = `project-description drop-shadow ${
-      iterator % 2 === 0 ? "left" : "right"
-    }`
-    projectDescription.innerText = project.attributes.description
-    projectTextContainer.appendChild(projectDescription)
+		const projectDescription = document.createElement("p")
+		projectDescription.classList = `project-description drop-shadow ${
+			iterator % 2 === 0 ? "left" : "right"
+		}`
+		projectDescription.innerText = project.attributes.description
+		projectTextContainer.appendChild(projectDescription)
 
-    const projectTextContainerBottom = document.createElement("div")
-    projectTextContainerBottom.classList = `project-text-container-bottom ${
-      iterator % 2 === 0 ? "left" : "right"
-    }`
-    projectTextContainer.appendChild(projectTextContainerBottom)
+		const projectTextContainerBottom = document.createElement("div")
+		projectTextContainerBottom.classList = `project-text-container-bottom ${
+			iterator % 2 === 0 ? "left" : "right"
+		}`
+		projectTextContainer.appendChild(projectTextContainerBottom)
 
-    if (project.attributes.technologies.data) {
-      const technologies = document.createElement("ul")
-      technologies.classList = `technologies horizontal-scroll ${
-        iterator % 2 === 0 ? "left" : "right"
-      }`
-      projectTextContainerBottom.appendChild(technologies)
+		if (project.attributes.technologies.data) {
+			const technologies = document.createElement("ul")
+			technologies.classList = `technologies horizontal-scroll ${
+				iterator % 2 === 0 ? "left" : "right"
+			}`
+			projectTextContainerBottom.appendChild(technologies)
 
-      //Loop through each technology
-      project.attributes.technologies.data.forEach((tech) => {
-        const technology = document.createElement("li")
-        technology.classList = "technology-item"
-        technology.innerText = tech.attributes.name
+			//Loop through each technology
+			project.attributes.technologies.data.forEach((tech) => {
+				const technology = document.createElement("li")
+				technology.classList = "technology-item"
+				technology.innerText = tech.attributes.name
 
-        technologies.appendChild(technology)
-      })
-    }
+				technologies.appendChild(technology)
+			})
+		}
 
-    const projectLinks = document.createElement("ul")
-    projectLinks.classList = "project-links"
-    projectTextContainerBottom.appendChild(projectLinks)
+		const projectLinks = document.createElement("ul")
+		projectLinks.classList = "project-links"
+		projectTextContainerBottom.appendChild(projectLinks)
 
-    const mediaLink = document.createElement("li")
-    mediaLink.classList = "media-link screenshots-link project-link"
-    mediaLink.innerText = "Screenshots"
-    projectLinks.appendChild(mediaLink)
+		const mediaLink = document.createElement("li")
+		mediaLink.classList = "media-link screenshots-link project-link"
+		mediaLink.innerText = "Screenshots"
+		projectLinks.appendChild(mediaLink)
 
-    if (project.attributes.liveSiteLink) {
-      const liveSiteLink = document.createElement("li")
-      liveSiteLink.classList = "project-link live-site-link"
+		if (project.attributes.liveSiteURL) {
+			const liveSiteLink = document.createElement("li")
+			liveSiteLink.classList = "project-link live-site-link"
 
-      const liveSiteAnchor = document.createElement("a")
-      liveSiteAnchor.innerText = "Live Site"
-      liveSiteAnchor.setAttribute("target", "blank")
-      liveSiteAnchor.classList = "project-link live-site-link"
-      liveSiteAnchor.setAttribute("href", project.attributes.liveSiteLink)
-      liveSiteLink.appendChild(liveSiteAnchor)
+			const liveSiteAnchor = document.createElement("a")
+			liveSiteAnchor.innerText = "Live Site"
+			liveSiteAnchor.setAttribute("target", "blank")
+			liveSiteAnchor.classList = "project-link live-site-link"
+			liveSiteAnchor.setAttribute("href", project.attributes.liveSiteURL)
+			liveSiteLink.appendChild(liveSiteAnchor)
 
-      projectLinks.appendChild(liveSiteLink)
-    }
+			projectLinks.appendChild(liveSiteLink)
+		}
 
-    if (project.attributes.githubLink) {
-      const githubLink = document.createElement("li")
-      githubLink.classList = "project-link github-link"
+		if (project.attributes.githubURL) {
+			const githubLink = document.createElement("li")
+			githubLink.classList = "project-link github-link"
 
-      const githubAnchor = document.createElement("a")
-      githubAnchor.innerText = "Github"
-      githubAnchor.setAttribute("target", "blank")
-      githubAnchor.classList = "project-link github-link"
-      githubAnchor.setAttribute("href", project.attributes.githubLink)
-      githubLink.appendChild(githubAnchor)
+			const githubAnchor = document.createElement("a")
+			githubAnchor.innerText = "Github"
+			githubAnchor.setAttribute("target", "blank")
+			githubAnchor.classList = "project-link github-link"
+			githubAnchor.setAttribute("href", project.attributes.githubURL)
+			githubLink.appendChild(githubAnchor)
 
-      projectLinks.appendChild(githubLink)
-    }
+			projectLinks.appendChild(githubLink)
+		}
 
-    const projects = document.querySelector(".projects")
-    projects.append(section)
+		const projects = document.querySelector(".projects")
+		projects.append(section)
 
-    iterator++
-  })
-
-  //   data.forEach((project) => console.log(project))
+		iterator++
+	})
 }
 
 fetchData("projects", "populate=*").then((data) => {
-  console.log(data)
-  populateProject(data)
+	populateProject(data)
 })
-
-// populateProject(fetchData("projects", "populate=screenshots"))
-
-console.log("connected")
